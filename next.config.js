1989334c86cc6db1995d90f655062a760eaffc81
KoheiKamijima <k.islandup@gmail.com>
2023-06-23 10:31:43 +0900

    add env config in module.export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAP_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
